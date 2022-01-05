6dbe99d3c4a1c75c950ed5763cb86250f0a987f7
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-05 23:03:39 +0530

    function a

--- script2.js
+++ script2.js
@@ -3,4 +3,7 @@ var sonic = document.getElementById("sonic");
 var set1X = 40;
 var set1Y = 0;
 
-function a() {}
+function a() {
+    set1X = set1X - 40;
+    sonic.style.backgroundPositionX = set1X + "px";
+}
